90228065c0918d89a8eb605821da95f77680683d
Alexandra Ibarra <alexasofia@hotmail.com>
2019-01-25 13:03:42 -0300

    start to fix matrix visualization

---
+++ src/vistorian-widgets-imports.ts
@@ -0,0 +1,5 @@
+/// <reference types="vistorian-widgets" />
+
+export import makeSlider = require("ui");
+export import TimeSlider = require("timeslider");
+export import glutils = require("glutils");
