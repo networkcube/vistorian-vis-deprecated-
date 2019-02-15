2a6167077362d9dd325562de314433ff063d35eb
Alexandra Ibarra <alexasofia@hotmail.com>
2019-02-15 17:03:38 +0100

    fix principal webpage

---
+++ classes/utils.ts
@@ -0,0 +1,17 @@
+export function getUrlVars() {
+    var vars: any = {};
+    var params: any = window.location.search.replace("?", "").split('&');
+    var tmp: any, value: any;
+    params.forEach(function (item: any) {
+        tmp = item.split("=");
+        value = decodeURIComponent(tmp[1]);
+        vars[tmp[0]] = value;
+    });
+    return vars;
+}
+
+/** Removes white space before and after a string */
+export function rtrim(s: string) {
+    return this.replace(/((\s*\S+)*)\s*/, "$1");
+}
+
