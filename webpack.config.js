33c2a5bf5eabbaf1c51a9c7562bdc8687d6c1814
Alexandra Ibarra <alexasofia@hotmail.com>
2019-03-18 16:17:26 +0100

    fix some bugs

--- webpack.config.js
+++ webpack.config.js
@@ -5,34 +5,7 @@ const CircularDependencyPlugin = require('circular-dependency-plugin');
 module.exports = {
     target: 'web',
     mode: 'production',
-    /*entry: './src/index.ts',
-    devtool: 'inline-source-map',
-    plugins: [
-        new CircularDependencyPlugin({
-            exclude: /a\.js|node_modules/,
-            failOnError: true,
-            cwd: process.cwd(),
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },*/
     devServer: {
         contentBase: './'
-    }/*,
-    output: {
-        library: "networkcube",
-        libraryTarget: "umd",
-        filename: 'vistorian-vis.js',
-        path: path.resolve(__dirname, 'lib')
-    }*/
+    }
 };
