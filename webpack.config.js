3f100d1d1b792de260a1c56d4742f7af759015c8
Alexandra Ibarra <alexasofia@hotmail.com>
2019-03-19 14:42:31 +0100

    add html of main web page

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,4 @@
-const path = require('path');
-const CircularDependencyPlugin = require('circular-dependency-plugin');
-//const webpack = require('webpack');
-
 module.exports = {
-    target: 'web',
-    mode: 'production',
     devServer: {
         contentBase: './'
     }
